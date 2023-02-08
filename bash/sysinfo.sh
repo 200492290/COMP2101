1e6ac5f0108834ed8540f9302c639821284fe3f1
Hetrajsinh Atodaria <200492290@student.georgianc.on.ca>
2023-02-08 08:58:17 -0500

    Lab 2 - Improvements in Sysinfo Script

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# Fully qualified domain name
-FQDN=$(hostname -f)
-echo "FQDN: $FQDN"
-
-# OS information
-echo "Host Information:"
-hostnamectl
-
-# IP information
-echo "IP Addresses:"
-ip -o -4 addr | awk '{print $4}'
-
-# File system information
-echo "Root Filesystem Status:"
-df /dev/sr1
+echo "Report for $(hostname)"
+echo "==============="
+echo "FQDN: $(hostname -f)"
+OS=$(lsb_release -d | awk '{print $2, $3, $4}')
+echo "Operating System name and version: $OS"
+IP=$(ip addr | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}' | cut -d/ -f1)
+echo "IP Address: $IP"
+ROOT_FS=$(df -h / | awk 'NR==2 {print $4}')
+echo "Root Filesystem Free Space: $ROOT_FS"
+echo "==============="
